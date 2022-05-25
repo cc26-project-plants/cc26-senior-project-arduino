0dcc08db388ce16d1d9f95227cf70caa864e1896
mikako-shirai <mkkshirai@gmail.com>
2022-05-25 12:31:31 +0900

    feat: add setup for Arduino

--- sensorSketch.ino
+++ sensorSketch.ino
@@ -1,9 +1,14 @@
-void setup() {
-  // put your setup code here, to run once:
 
+int temperatureInput = 0;
+int humidityInput = 0;
+int mistureInput = 0;
+
+void setup() {
+  Serial.begin(115200);
+  while (!Serial);
+  Serial.println("Arduino running on ATmega2560");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  // get and modify data 
 }
