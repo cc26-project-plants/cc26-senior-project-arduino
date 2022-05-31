080e8dc56b25f74a8ebc6f2764e979e8f2cdf22b
Thomas Ryan <DWTrip@gmail.com>
2022-05-31 15:38:56 +0900

    Changing branches for new feature

--- ESP8266/ESP8266.ino
+++ ESP8266/ESP8266.ino
@@ -3,15 +3,41 @@
 #include "DHT.h"
 #define DHTPIN D2
 #define DHTTYPE DHT22
-DHT dht(DHTPIN, DHTTYPE);
-
-
 
 // Set WiFi credentials
-#define WIFI_SSID "codechrysalis_2.4ghz"
-#define WIFI_PASS "foreverbekind"
+//Code Chrysalis
+//#define WIFI_SSID "codechrysalis_2.4ghz"
+//#define WIFI_PASS "foreverbekind"
+//Home
+#define WIFI_SSID "ASUS_D0"
+#define WIFI_PASS "FFFFFFFFFF1"
 
-auto& arduino = Serial1;
+//Constants *****************************************************
+DHT dht(DHTPIN, DHTTYPE);
+
+//Class creation & object creation (THIS MAY BE WRONG!!)****************************************
+
+  class Plant {
+  public:
+    int soilWaterLevel;
+    int lightLevel;
+    float humidLevel;
+    float temp;
+
+    //methods
+    void updateSoilWaterLevel(int value){
+      soilWaterLevel = value;
+    }
+};
+
+Plant fakePlant;
+fakePlant.soilWaterLevel = 1;
+fakePlant.lightLevel = 20;
+fakePlant.humidLevel = 3.0;
+fakePlant.temp = 10.0;
+}
+
+//Setup function ***********************************************
 
 void setup() {
   // Setup serial port
@@ -40,28 +66,29 @@ void setup() {
   //DHT INIT
   dht.begin();
   delay(2000);
-}
+
+//Main program *****************************************************************************
 
 void loop() {
-  Serial.println("Waiting for input");
-  delay(500);
-  waitDelay(100);
-  waitDelay(100);
-  waitDelay(100);
+
+  
+  Serial.print("Fake plant Soil water level: ");
+  Serial.println(fakePlant.soilWaterLevel);
+  delay(2000);
+  Serial.println("Updating soil water level...");
+  fakePlant.updateSoilWaterLevel(500);
   delay(1000);
+  Serial.print("Fake plant Soil water level is now: ");
+  Serial.println(fakePlant.soilWaterLevel);
+  delay(2000);
 
-  Serial.println("Checking Serial comm");
-  //Try to talk to arduino
-  if (Serial.available() > 0) {
-    // read the incoming byte:
-    int incomingByte = Serial.read();
-
-    // say what you got:
-    Serial.print("I received: ");
-    Serial.println(incomingByte);
-  }
+  
+  Serial.println(analogRead(A0));
+  printTemp();
+  delay(500);
 }
 
+//Functions **********************************************************************************
 void waitDelay(int time) {
   delay(time);
   Serial.print(".");
@@ -94,3 +121,8 @@ void printHumid(){
   Serial.println("%");
  }
 }
+
+void updateLight(){
+   
+  
+ }
