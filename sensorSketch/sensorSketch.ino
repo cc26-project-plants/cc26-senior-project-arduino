c43ea56b7c82c7e31f4e8c4824ec1d0de128ec96
mikako-shirai <mkkshirai@gmail.com>
2022-05-31 16:09:38 +0900

    refactor: update previous code

--- sensorSketch/sensorSketch.ino
+++ sensorSketch/sensorSketch.ino
@@ -1,38 +1,28 @@
 
-// Analog pins
+// analog pins
 int lightPin = A0;
 int soilWaterPin = A2;
 
-// Digital pins
+// digital pins
 int pumpPowerPin = 4;
 int ledPin = 13;
 
-// Humidity/temperature pin
+// humidity/temperature pin
 #include "DHT.h";
-//#include "DHT_U.h";
-//#include "Adafruit_Sensor.h";
 #define DHTPIN 2
 #define DHTTYPE DHT22
 DHT dht(DHTPIN, DHTTYPE);
 
-// Sensor reading variables
 int lightLevel;
 int soilWaterLevel;
-int humidity;
-int temperature;
-
-//unsigned int minLight = 65536;
-//unsigned int maxLight = 0;
-//float miniTemp = 5505;
-//float maxTemp = 0;
-//float minHumid = 100;
-//float maxHumid = 0;
+float humidity;
+float temperature;
 
 void setup() {
   Serial.begin(115200);
   dht.begin();
 
-  //set pin modes
+  // set pin modes
   pinMode(lightPin, INPUT);
   pinMode(soilWaterPin, INPUT);
   pinMode(ledPin, OUTPUT);
@@ -40,7 +30,7 @@ void setup() {
 
 void loop() {
   readLightLevel();
-  writeLedBrightness();
+ writeLedBrightness();
 
   readSoilWaterLevel();
 
@@ -50,41 +40,37 @@ void loop() {
   delay(1000);
 }
 
-// Functions
+// functions
 void readLightLevel() {
   lightLevel = analogRead(lightPin);
   
-//  Serial.print("Light Level: ");
-//  Serial.println(lightLevel);
+  Serial.print("Light Level: ");
+  Serial.println(lightLevel);
 }
 
 void readSoilWaterLevel() {
   soilWaterLevel = analogRead(soilWaterPin);
   
-//  Serial.print("Soil Water Level: ");
-//  Serial.println(soilWaterLevel);
+  Serial.print("Soil Water Level: ");
+  Serial.println(soilWaterLevel);
 }
 
 void readHumidityTemperature() {
-  humidity = dht.readHumidity()  * 10;
-  temperature = dht.readTemperature()  * 10;
+  humidity = dht.readHumidity();
+  temperature = dht.readTemperature();
   
-  Serial.print("Humidity: ");
+  Serial.print("Humidity Level: ");
   Serial.println(humidity);
   Serial.print("Temperature: ");
   Serial.println(temperature);
 }
 
 void writeLedBrightness() {
-  // LED 0 - 255
   int bright = 950;
   int dark = 1000;
   int ledStep = 255 / (dark - bright);
+  
   int lightInput = lightLevel > dark ? dark : lightLevel < bright ? bright : lightLevel;
-
   int brightness = ledStep * (lightInput - bright);
   analogWrite(ledPin, brightness);
-  
-//  Serial.print("LED Brightness: ");
-//  Serial.println(brightness);
 }
