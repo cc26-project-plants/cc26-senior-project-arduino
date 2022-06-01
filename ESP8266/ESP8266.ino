a31ef93dfc1d6da6089d45966b9c9a9ddf7b9679
mikako-shirai <mkkshirai@gmail.com>
2022-06-01 11:35:17 +0900

    style: organize code

--- ESP8266/ESP8266.ino
+++ ESP8266/ESP8266.ino
@@ -1,18 +1,18 @@
-// Library
+#include <ESP8266HTTPClient.h>
 #include <ESP8266WiFi.h>
-#include "DHT.h"
+#include <DHT.h>
+#include <ArduinoJson.h>
 
-// Set temperature humidity sensor
-#define DHTPIN D2
-#define DHTTYPE DHT22
-
-// Set WiFi credentials
 // Code Chrysalis
 #define WIFI_SSID "codechrysalis_2.4ghz"
 #define WIFI_PASS "foreverbekind"
 // Home
 // #define WIFI_SSID "ASUS_D0"
 // #define WIFI_PASS "FFFFFFFFFF1"
+
+#define DHTPIN D2
+#define DHTTYPE DHT22
+DHT dht(DHTPIN, DHTTYPE);
 
 // Class *************************************************************
 class Plant;
@@ -27,6 +27,7 @@ public:
   void updateLightLevel() {
     lightLevel = analogRead(A0);
   }
+  
   int getLightLevel() {
     return lightLevel;  
   }
@@ -35,6 +36,7 @@ public:
   void updateSoilWaterLevel(int value) {
     soilWaterLevel = value;
   }
+  
   int getSoilWaterLevel() {
     return soilWaterLevel;  
   }
@@ -43,6 +45,7 @@ public:
   void updateTemperature() {
     temperature = dht.readTemperature();
   }
+  
   int getTemperature() {
     return temperature;  
   }
@@ -51,6 +54,7 @@ public:
   void updateHumidityLevel() {
     humidityLevel = dht.readHumidity();
   }
+  
   int getHumidityLevel() {
     return humidityLevel;  
   }
@@ -64,20 +68,24 @@ public:
   }
 
   void printAll() {
-    Serial.println("Light Level: " + lightLevel);
-    Serial.println("Soil Water Level: " + soilWaterLevel);
-    Serial.println("Temperature: "+ temperature + "~C");
-    Serial.println("Humidity: " + temperature + "%");
+    Serial.println("Light Level: " + String(lightLevel));
+    Serial.println("Soil Water Level: " + String(soilWaterLevel));
+    Serial.println("Temperature: " + String(temperature) + "~C");
+    Serial.println("Humidity: " + String(temperature) + "%");
   }
 };
 
-// Constants *********************************************************
-DHT dht(DHTPIN, DHTTYPE);
+// Declare instances/variables *************************************************
+WiFiClient client;
+HTTPClient http;
 Plant fakePlant;
+
+String URL = "http://happa-26-backend.an.r.appspot.com/plantStats/";
+String PLANT_ID = "VAlAa3aEtub3qSw7SIjz";
 
 // Setup function ****************************************************
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
 
   WiFi.begin(WIFI_SSID, WIFI_PASS);
   setUpWiFi();
@@ -90,19 +98,24 @@ void setup() {
 void loop() {
   fakePlant.updateAll();
   fakePlant.printAll();
-  waitDelay(1000);
+  waitDelay(3000);
 
-  Serial.println("TEST");
+  postRequest();
+  
+  // Delay for 5 minutes
+  delay(300000);
+  
+  // Delay for 1 hour
+  // delay(3600000);
 }
 
 // Functions *********************************************************
 void setUpWiFi() {
-  Serial.println("Connecting to " + WIFI_SSID);
-
   while (WiFi.status() != WL_CONNECTED) {
     waitDelay(200);
   }
-  Serial.println("Connected! IP address: " + WiFi.localIP());
+  Serial.print("Wifi connected. IP address: ");
+  Serial.println(WiFi.localIP());
 }
 
 void waitDelay(int time) {
@@ -114,4 +127,31 @@ void waitDelay(int time) {
   Serial.print(".");
   delay(time);
   Serial.println();
+}
+
+void postRequest() {
+  Serial.println("Sending post request...");
+  
+  String jString = "{\"soilWaterLevel\":" + String(fakePlant.soilWaterLevel)
+                   + ",\"lightLevel\":" + String(fakePlant.lightLevel)
+                   + ",\"humidityLevel\":" + String(fakePlant.humidityLevel)
+                   + ",\"temperature\":" + String(fakePlant.temperature)
+                   + "}";
+  Serial.println("JSON string to be sent : " + jString);
+  
+  http.begin(client, URL + PLANT_ID);
+  http.addHeader("Content-Type", "application/json");
+
+  int httpCode = http.POST(jString);
+  if (httpCode > 0) {
+    Serial.printf("[HTTP] POST... code: %d\n", httpCode);
+
+    if (httpCode == HTTP_CODE_OK) {
+      const String& payload = http.getString();
+      Serial.println("received payload:\n<<" + String(payload) + ">>\n");
+    }
+  } else {
+    Serial.printf("[HTTP] POST... failed, error: %s\n", http.errorToString(httpCode).c_str());
+  }
+  http.end();
 }
