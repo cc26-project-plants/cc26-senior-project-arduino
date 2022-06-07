00581f8044b011bad717d3a8bc84a89e1a530b6e
mikako-shirai <mkkshirai@gmail.com>
2022-06-07 14:13:27 +0900

    chore: change pins

--- ESP8266/ESP8266.ino
+++ ESP8266/ESP8266.ino
@@ -10,7 +10,7 @@
 #define LED_PIN D2
 #define DHT_PIN D3
 #define PUMP_POWER_PIN D5
-#define LIGHT_POWER_PIN D6
+#define LIGHT_POWER_PIN A1
 #define DHTTYPE DHT22
 DHT dht(DHT_PIN, DHTTYPE);
 Adafruit_ADS1115 analogChip;
@@ -28,9 +28,9 @@ const String URL = "http://happa-26-backend.an.r.appspot.com/plantStats/";
 const String PLANT_ID = "VAlAa3aEtub3qSw7SIjz";  // Thomas: "wdNtSRStxaQU9gc2QWM7"
 
 // MQTT
-#define MQTT_SEVER "broker.mqtt-dashboard.com"
-#define TOPIC "wemos"
-#define TIME_BETWEEN_MESSAGES 1000 * 5
+//#define MQTT_SEVER "broker.mqtt-dashboard.com"
+//#define TOPIC "wemos"
+//#define TIME_BETWEEN_MESSAGES 1000 * 5
 
 // ************************************************************* Class
 class Plant {
@@ -93,7 +93,7 @@ public:
 Plant plantTest;
 WiFiClient clientEsp;
 HTTPClient http;
-PubSubClient clientMqtt(clientEsp);
+//PubSubClient clientMqtt(clientEsp);
 
 unsigned long lastMsg = 0;
 int value = 0;
@@ -116,17 +116,17 @@ void setup() {
   setupHTTP();
   
   // MQTT
-  clientMqtt.setServer(MQTT_SERVER, 1883);
-  clientMqtt.setCallback(callback);
+//  clientMqtt.setServer(MQTT_SERVER, 1883);
+//  clientMqtt.setCallback(callback);
 }
 
 void loop() {
   plantTest.updateAll();
   plantTest.printAll();
 
-  checkMQTT();
+//  checkMQTT();
   
-  message();
+//  message();
 
   waitDelay(3000);
 
@@ -166,12 +166,12 @@ void setupHTTP() {
   http.begin(clientEsp, URL + PLANT_ID);
   http.addHeader("Content-Type", "application/json");
 }
-
-void checkMQTT() {
-  // confirm still connected to mqtt server
-  if (!clientMqtt.connected()) reconnect();
-  clientMqtt.loop();
-}
+//
+//void checkMQTT() {
+//  // confirm still connected to mqtt server
+//  if (!clientMqtt.connected()) reconnect();
+//  clientMqtt.loop();
+//}
 
 // ************************************************ HTTP
 void postRequest() {
@@ -207,83 +207,83 @@ String buildJSON() {
 }
 
 // ************************************************ MQTT
-void callback(char* topic, byte* payload, unsigned int length) {
-  Serial.println("[MQTT] message received (" + String(topic) + ")\n");
-  
-  for (int i = 0; i < length; i++) {
-    Serial.print((char)payload[i]);
-  }
-  Serial.println();
-
-  // Switch on the LED if an 1 was received as first character (LOW is the voltage level)
-  if ((char)payload[0] == '1') {
-    digitalWrite(BUILTIN_LED, LOW);
-  } else {
-    digitalWrite(BUILTIN_LED, HIGH);
-  }
-}
-
-String createClientID() {
-  uint8_t mac[6];
-  WiFi.macAddress(mac);
-  String clientId = "esp-" + macToStr(mac);
-  return clientId;
-}
-
-String macToStr(const uint8_t* mac) {
-  String str;
-  for (int i = 0; i < 6; ++i) {
-    str += String(mac[i], 16);
-    if (i < 5) str += ':';
-  }
-  return str;
-}
-
-void reconnect() {
-  Serial.println("[MQTT] connecting MQTT");
-  
-  while (!clientMqtt.connected()) {
-    Serial.println(".");
-    
-    String clientId = createClientID();
-    clientId += "-";
-    clientId += String(micros() & 0xff, 16);
-
-    if (clientMqtt.connect(clientId.c_str())) {
-      Serial.println("[MQTT] connected");
-
-      // Once connected, publish an announcement
-      clientMqtt.publish(TOPIC, ("connected " + createClientID()).c_str(), true);
-      
-      // and resubscribe
-      String subscription;
-      subscription += String(TOPIC) + "/" + createClientID() + "/in";
-      clientMqtt.subscribe(subscription.c_str() );
-      Serial.println("[MQTT] subscribed to : " + subscription);
-    } else {
-      Serial.println("[MQTT] error, rc=" + String(clientMqtt.state()) + ", wifi=" + String(WiFi.status()));
-      Serial.println("try again in 5 seconds");
-      delay(5000);
-    }
-  }
-}
-
-void message() {
-  long now = millis();
-  
-  if (now - lastMsg > TIME_BETWEEN_MESSAGES) {
-    lastMsg = now;
-    value += 1;
-
-    String payload = "{\"micros\":" + String(micros())
-                     + ",\"counter\":" + String(value)
-                     + ",\"client\":" + createClientID()
-                     + "}";
-    String pubTopic;
-    pubTopic += String(TOPIC) + "/" + createClientID() + "/out";
-
-    Serial.println("Publish topic: " + pubTopic);
-    Serial.println("Publish message: " + payload);
-    clientMqtt.publish((char*) pubTopic.c_str(), (char*) payload.c_str(), true);
-  }
-}
+//void callback(char* topic, byte* payload, unsigned int length) {
+//  Serial.println("[MQTT] message received (" + String(topic) + ")\n");
+//  
+//  for (int i = 0; i < length; i++) {
+//    Serial.print((char)payload[i]);
+//  }
+//  Serial.println();
+//
+//  // Switch on the LED if an 1 was received as first character (LOW is the voltage level)
+//  if ((char)payload[0] == '1') {
+//    digitalWrite(BUILTIN_LED, LOW);
+//  } else {
+//    digitalWrite(BUILTIN_LED, HIGH);
+//  }
+//}
+//
+//String createClientID() {
+//  uint8_t mac[6];
+//  WiFi.macAddress(mac);
+//  String clientId = "esp-" + macToStr(mac);
+//  return clientId;
+//}
+//
+//String macToStr(const uint8_t* mac) {
+//  String str;
+//  for (int i = 0; i < 6; ++i) {
+//    str += String(mac[i], 16);
+//    if (i < 5) str += ':';
+//  }
+//  return str;
+//}
+//
+//void reconnect() {
+//  Serial.println("[MQTT] connecting MQTT");
+//  
+//  while (!clientMqtt.connected()) {
+//    Serial.println(".");
+//    
+//    String clientId = createClientID();
+//    clientId += "-";
+//    clientId += String(micros() & 0xff, 16);
+//
+//    if (clientMqtt.connect(clientId.c_str())) {
+//      Serial.println("[MQTT] connected");
+//
+//      // Once connected, publish an announcement
+//      clientMqtt.publish(TOPIC, ("connected " + createClientID()).c_str(), true);
+//      
+//      // and resubscribe
+//      String subscription;
+//      subscription += String(TOPIC) + "/" + createClientID() + "/in";
+//      clientMqtt.subscribe(subscription.c_str() );
+//      Serial.println("[MQTT] subscribed to : " + subscription);
+//    } else {
+//      Serial.println("[MQTT] error, rc=" + String(clientMqtt.state()) + ", wifi=" + String(WiFi.status()));
+//      Serial.println("try again in 5 seconds");
+//      delay(5000);
+//    }
+//  }
+//}
+//
+//void message() {
+//  long now = millis();
+//  
+//  if (now - lastMsg > TIME_BETWEEN_MESSAGES) {
+//    lastMsg = now;
+//    value += 1;
+//
+//    String payload = "{\"micros\":" + String(micros())
+//                     + ",\"counter\":" + String(value)
+//                     + ",\"client\":" + createClientID()
+//                     + "}";
+//    String pubTopic;
+//    pubTopic += String(TOPIC) + "/" + createClientID() + "/out";
+//
+//    Serial.println("Publish topic: " + pubTopic);
+//    Serial.println("Publish message: " + payload);
+//    clientMqtt.publish((char*) pubTopic.c_str(), (char*) payload.c_str(), true);
+//  }
+//}
